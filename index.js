cb814b6341f96b9df733a370bcc08b4f406ccb5d
Kevin <g95wang@gmail.com>
2021-03-13 23:18:13 -0500

    fix

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const Timidity = require("timidity");
 
-getAudioContext().resume();
 const player = new Timidity();
-player.load("./music.mid");
-player.play();
+function playMusic() {
+  player.load("./music.mid");
+  // getAudioContext().resume();
+  player.play();
 
-// player.on("playing", () => {
-//   console.log(player.duration); // => 351.521
-// });
+  player.on("playing", () => {
+    console.log(player.duration); // => 351.521
+  });
+}
+
+module.exports = { playMusic: playMusic };
